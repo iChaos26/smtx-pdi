69e8fb1ff17dec70e7295493bb6a24cfe45f7f70
joaorodriguessemantix <joao.rodrigues@semantix.com.br>
2020-05-18 00:29:31 -0300

    Initial setup, running some tests

---
+++ test-flask.py
@@ -0,0 +1,8 @@
+"""Filename: hello-world.py
+  """
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/users/<string:username>')
+def hello_world(username=None):
+    return("Hello {}!".format(username))
